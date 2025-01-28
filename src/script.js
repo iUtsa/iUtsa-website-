5c6caa34ade2e3d21a693f311659c66d30036e6c
Arnab Das Utsa <144443522+iUtsa@users.noreply.github.com>
2025-01-28 18:21:39 +0000

    smooth scrolling

--- src/script.js
+++ src/script.js
@@ -199,3 +199,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+//smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
